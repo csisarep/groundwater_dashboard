969278a327744974d542b1cb56959e90ce8ff62e
Saralkarki <Saralkarki@users.noreply.github.com>
2022-03-04 12:12:05 +0545

    main branch

--- groundWaterProject/settings.py
+++ groundWaterProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '$@#&nut@#%9e1uz)i_cv=_g&-dlwe#_okyb^@8gknlp!a*1+4m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','gw-nepal.com','www.gw-nepal.com']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','gw-nepal.com','www.gw-nepal.com','*']
 
 
 # Application definition
